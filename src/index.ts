cd2c2dc7a18eba25b18d821af1832272c7d34c78
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-29 19:16:11 -0300

    Commit29/05 Pequeña mejora al ejercicio.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ let dimensionArreglo: number = Number(
   prompt("Ingrese la cantidad de butacas que hay en el cine")
 );
 let butacasArreglo: boolean[] = new Array(dimensionArreglo);
-let indice,
-  resultado: number = 0;
+let indice: number = 0;
+let resultadoFalse: number = 0;
+let resultadoTrue: number = 0;
 
 for (indice = 0; indice < butacasArreglo.length; indice++) {
   if (Math.random() < 0.5) {
     console.log("La butaca está ocupada");
+    resultadoTrue++;
   } else {
     console.log("La butaca está vacía");
-    resultado++;
+    resultadoFalse++;
   }
 }
-console.log(`Hay ${resultado} butacas vacías`);
+console.log(`Hay ${resultadoTrue} butacas ocupadas`);
+console.log(`Hay ${resultadoFalse} butacas vacías`);
