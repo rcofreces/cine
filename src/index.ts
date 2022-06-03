afed1e3aedcd98547f509b470c0ccca55feac23a
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-06-03 14:55:51 -0300

    commit03/06 Corregí ejercicio luego de la devolución del tutor.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
+/*• Diseñar un algoritmo que recorra las
+butacas de una sala de cine y determine
+cuántas butacas desocupadas hay
+• Suponga que para modelar este
+problema, se utiliza un arreglo con
+valores lógicos
+• La presencia de un valor verdadero (true) en
+el arreglo indica que la butaca está ocupada
+• La presencia de un valor falso (false) en el
+arreglo indica que la butaca está desocupada*/
+
 let dimensionArreglo: number = Number(
   prompt("Ingrese la cantidad de butacas que hay en el cine")
 );
 let butacasArreglo: boolean[] = new Array(dimensionArreglo);
-let indice: number = 0;
-let resultadoFalse: number = 0;
-let resultadoTrue: number = 0;
+let contarVacias: number = 0;
+let contarOcupadas: number = 0;
+let i: number = 0;
 
-for (indice = 0; indice < butacasArreglo.length; indice++) {
-  if (Math.random() < 0.5) {
+for (i = 0; i < butacasArreglo.length; i++) {
+  butacasArreglo[i] = Boolean(Math.floor(Math.random() * -2 + 2));
+}
+for (i = 0; i < butacasArreglo.length; i++) {
+  if (butacasArreglo[i] === true) {
     console.log("La butaca está ocupada");
-    resultadoTrue++;
+    contarOcupadas = contarOcupadas + 1;
   } else {
     console.log("La butaca está vacía");
-    resultadoFalse++;
+    contarVacias = contarVacias + 1;
   }
 }
-console.log(`Hay ${resultadoTrue} butacas ocupadas`);
-console.log(`Hay ${resultadoFalse} butacas vacías`);
+console.log(`Hay ${contarOcupadas} butacas ocupadas`);
+console.log(`Hay ${contarVacias} butacas vacías`);
